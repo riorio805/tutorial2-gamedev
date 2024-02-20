31f3d99a0bd8e130db0fd12901ca72fb662cb388
riorio805 <sefrianojieftara@gmail.com>
2024-02-20 10:14:49 +0700

    bbbbbbbb

--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+var touched = false
 
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
-	if (body.name == "BlueShip" or body.name == "GreenShip"):
+	if (body.name == "BlueShip" or body.name == "GreenShip") and not touched:
+		touched = true
 		print("Reached objective!")
 		print("Current level: " + get_tree().get_current_scene().get_name())
-		OS.delay_msec(1000)
+		yield(get_tree().create_timer(1), "timeout")
 		
 		if get_tree().get_current_scene().get_name() == "MainLevel":
 			get_tree().change_scene("res://scenes/Level2.tscn")
